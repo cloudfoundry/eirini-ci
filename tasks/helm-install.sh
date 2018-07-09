23562c6649f0d2941dd9f03bff32e277c901e4ab
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-07-09 15:33:33 +0200

    push updates

--- tasks/helm-install.sh
+++ tasks/helm-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "TODO"
+mkdir -p ~/.kube
+echo "$KUBE_CONF" > ~/.kube/config
+
+helm install \
+	--set-string \
+	  ingress.opi.host=\"eirini-opi."$KUBE_ENDPOINT"\",ingress.registry.host=\"eirini-registry."$KUBE_ENDPOINT"\" \
+	--debug \
+	--name \
+	./eirini-release/kube-release/helm/eirini
