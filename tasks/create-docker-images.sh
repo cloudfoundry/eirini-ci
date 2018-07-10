d61dbbc6c9fccdc790c45f6e394b7370fb26e9b0
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-07-10 08:23:54 +0200

    start docker deamon

--- tasks/create-docker-images.sh
+++ tasks/create-docker-images.sh
@@ -2,7 +2,14 @@
 
 set -ex
 
-./eirini-release/kube-release/docker/generate-docker-image.sh "$TAG" #TODO: modify script in eirini-release/kube-release/docker to use TAG env variable
+# Start Docker Daemon (and set a trap to stop it once this script is done)
+echo 'DOCKER_OPTS="--data-root /scratch/docker --max-concurrent-downloads 10"' > /etc/default/docker
+service docker start
+service docker status
+trap 'service docker stop' EXIT
+sleep 10
+
+./eirini-release/kube-release/docker/generate-docker-image.sh "$TAG"
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 docker push "eirini/opi:$TAG"
