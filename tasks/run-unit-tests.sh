0136636f50c71d35325dfaf3d5774d17154553f9
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-07-18 09:22:01 +0200

    add GOPATH to run-unit-test job; move set-iptable-rules to right file
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/run-unit-tests.sh
+++ tasks/run-unit-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+
+# shellcheck disable=SC2034
+GOPATH=$(readlink -f eirini-source)
+
 readonly WORKSPACE="$(readlink -f eirini-source)/src/code.cloudfoundry.org/eirini"
 cd "$WORKSPACE"
 ginkgo -r -keepGoing --skipPackage=launcher,recipe --skip="Desiring some LRPs|Desiretask"
