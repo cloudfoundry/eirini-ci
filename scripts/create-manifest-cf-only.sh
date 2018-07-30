708a4fac312dcc0ee232e3a053d53e79b113b8bf
Georgi Dankov <gddankov@gmail.com>
2018-07-30 11:16:19 +0200

    fix path to use-bosh-dns ops file
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- scripts/create-manifest-cf-only.sh
+++ scripts/create-manifest-cf-only.sh
@@ -5,7 +5,7 @@ bosh interpolate cf-deployment/cf-deployment.yml \
     --ops-file cf-deployment/operations/experimental/enable-bpm.yml \
     --ops-file cf-deployment/operations/use-compiled-releases.yml \
     --ops-file cf-deployment/operations/bosh-lite.yml \
-    --ops-file cf-deployment/operations/experimental/use-bosh-dns.yml \
+    --ops-file cf-deployment/operations/use-bosh-dns.yml \
     --ops-file eirini-release/operations/capi-dev-version.yml \
     --ops-file eirini-release/operations/enable-opi.yml \
     --ops-file cf-deployment/iaas-support/softlayer/add-system-domain-dns-alias.yml \
