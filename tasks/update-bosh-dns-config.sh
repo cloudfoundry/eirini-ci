4d517e011b02b93c83e2f533d45ea80bddbeb8c2
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-07-31 11:24:19 +0200

    add vars store to update-runtime config
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/update-bosh-dns-config.sh
+++ tasks/update-bosh-dns-config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+DIRECTOR_PATH="state/environments/softlayer/director/$BOSH_LITE_NAME"
+
 . 1-click/tasks/bosh-login.sh
 
-bosh2 -n update-runtime-config bosh-deployment/runtime-configs/dns.yml --name=dns
+bosh2 -n update-runtime-config bosh-deployment/runtime-configs/dns.yml \
+	--name=dns \
+	--vars-store "$DIRECTOR_PATH/cf-deployment/vars.yml"
