d2df88527b9079063bb20c8633775501c97fceaa
Georgi Dankov <gddankov@gmail.com>
2018-08-10 15:31:06 +0200

    rebase capi release
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/rebase-capi.sh
+++ tasks/rebase-capi.sh
@@ -2,15 +2,41 @@
 
 set -x
 
-pushd capi/src/cloud_controller_ng/ || exit 1
-	git pull origin eirini
-popd || exit 1
+readonly CAPI_REMOTE="http://github.com/cloudfoundry/capi-release.git"
 
-cp -r capi/. capi-modified
+main(){
+	add_remote
+  rebase_capi
+	update_submodule
+	commit
+}
 
-pushd capi-modified || exit 1
-  git add src/cloud_controller_ng/
-  git config --global user.email "eirini@cloudfoundry.org"
-  git config --global user.name "Come-On Eirini"
-  git commit --all --message "Bump cloud_controller_ng submodule"
-popd || exit 1
+add_remote(){
+  git remote add original "$CAPI_REMOTE"
+}
+
+rebase_capi(){
+  pushd capi || exit 1
+    git fetch original "refs/notes/*:refs/notes/*"
+    git pull --rebase=preserve original master
+  popd || exit 1
+}
+
+update_submodule(){
+  pushd capi/src/cloud_controller_ng/ || exit 1
+    git pull origin eirini
+  popd || exit 1
+}
+
+commit(){
+  cp -r capi/. capi-modified
+
+  pushd capi-modified || exit 1
+    git add src/cloud_controller_ng/
+    git config --global user.email "eirini@cloudfoundry.org"
+    git config --global user.name "Come-On Eirini"
+    git commit --all --message "Bump cloud_controller_ng submodule"
+  popd || exit 1
+}
+
+main "$@"
