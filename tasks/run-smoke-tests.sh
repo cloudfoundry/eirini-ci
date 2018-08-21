8d82bcd6aef3ad3a3d299af7c02beb66c572b2d9
Mario Nitchev <marionitchev@gmail.com>
2018-08-21 13:34:54 +0300

    Refactor smokes and cats scripts
    * Extract common functions
    * Disable v3 tests in cats
    * Enable log-cache in cats
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/run-smoke-tests.sh
+++ tasks/run-smoke-tests.sh
@@ -1,28 +1,13 @@
 #!/bin/bash -ex
 
+source ./ci-resources/scripts/tests-env-setup.sh
+
 main() {
     set_up_gopath
     set_up_kubectl
     set_up_logcache
     configure_tests
     run_tests
-}
-
-set_up_gopath() {
-    mkdir -p gopath/src/github.com/cloudfoundry
-    export GOPATH=$PWD/gopath
-    export PATH=$PATH:$GOPATH/bin
-}
-
-set_up_logcache() {
-    echo "Current CF version:"
-    cf version
-    cf install-plugin -r CF-Community "log-cache" -f
-
-    export CF_PLUGIN_HOME=$HOME
-    echo "CF_PLUGIN_HOME is: ${CF_PLUGIN_HOME}"
-
-    export LOG_CACHE_ADDR="http://$(kubectl get service log-cache-reads -o jsonpath='{$.status.loadBalancer.ingress[0].ip}' -n oratos):8081"
 }
 
 configure_tests() {
@@ -56,27 +41,6 @@ configure_tests() {
 EOF
     export CONFIG="$(readlink -nf config.json)"
 }
-
-
-set_up_kubectl() {
-    echo "$KUBE_CONF" > kube.yml
-    export KUBECONFIG=$PWD/kube.yml
-
-    echo "KUBECONFIG is at: ${KUBECONFIG}"
-
-    echo "Installing kubectl..."
-
-    apt-get update && sudo apt-get install -y apt-transport-https
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-    touch /etc/apt/sources.list.d/kubernetes.list
-    echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-    apt-get update
-    apt-get install -y kubectl
-
-    echo "kubectl installed successfully!"
-    kubectl version
-}
-
 
 run_tests() {
     cp -a cf-smoke-tests $GOPATH/src/github.com/cloudfoundry/
