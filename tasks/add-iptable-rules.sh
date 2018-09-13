597c49315b6b8482fd5b41627a155f094ef41606
Mario Nitchev <marionitchev@gmail.com>
2018-09-13 17:40:33 +0300

    Add staging complete iptables rule
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- tasks/add-iptable-rules.sh
+++ tasks/add-iptable-rules.sh
@@ -16,11 +16,13 @@ set_rules() {
   local opi_rule="-p tcp --dport 8085 -j DNAT --to $EIRINI_IP:8085"
   local registry_rule="-p tcp --dport 8080 -j DNAT --to $EIRINI_IP:8080"
   local cc_uploader_rule="-p tcp --dport 9091 -j DNAT --to $API_BOSH_IP:9091"
+  local cc_staging_complete_rule="-p tcp --dport 9022 -j DNAT --to $API_BOSH_IP:9022"
 
   check_and_set "$nats_rule"
   check_and_set "$opi_rule"
   check_and_set "$registry_rule"
   check_and_set "$cc_uploader_rule"
+  check_and_set "$cc_staging_complete_rule"
 }
 
 # shellcheck disable=SC2029
