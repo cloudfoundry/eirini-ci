2b7937de603b2a2c8823ffec096a7e856849895f
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-09-13 17:24:21 +0300

    Add cc-uploader iptables rule. Add scheme to eirini_address
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/add-iptable-rules.sh
+++ tasks/add-iptable-rules.sh
@@ -3,6 +3,8 @@
 readonly DIRECTOR_DIR="state/environments/softlayer/director/$DIRECTOR_NAME"
 readonly JUMPBOX_KEY="$DIRECTOR_DIR/jumpbox.key"
 readonly DIRECTOR_IP="$(cat "$DIRECTOR_DIR/ip")"
+readonly API_BOSH_IP="10.244.0.135"
+readonly NATS_BOSH_IP="10.244.0.129"
 
 main() {
   chmod 600 "$JUMPBOX_KEY"
@@ -10,13 +12,15 @@ main() {
 }
 
 set_rules() {
-  local nats_rule="-p tcp --dport 4222 -j DNAT --to 10.244.0.129:4222"
+  local nats_rule="-p tcp --dport 4222 -j DNAT --to $NATS_BOSH_IP:4222"
   local opi_rule="-p tcp --dport 8085 -j DNAT --to $EIRINI_IP:8085"
   local registry_rule="-p tcp --dport 8080 -j DNAT --to $EIRINI_IP:8080"
+  local cc_uploader_rule="-p tcp --dport 9091 -j DNAT --to $API_BOSH_IP:9091"
 
   check_and_set "$nats_rule"
   check_and_set "$opi_rule"
   check_and_set "$registry_rule"
+  check_and_set "$cc_uploader_rule"
 }
 
 # shellcheck disable=SC2029
