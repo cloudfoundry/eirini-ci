c694d376b7f4982c84116f2be7ab604fbeef40e7
Georgi Dankov <gddankov@gmail.com>
2018-10-09 17:54:00 +0300

    Use dedicated docker image for golangci-lint
    [#160717197]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- tasks/run-static-checks.sh
+++ tasks/run-static-checks.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-set -x -e
+set -euo pipefail
+IFS=$'\n\t'
 
 GOPATH=$(readlink -f eirini-source)
-readonly PROJECT_DIR="$GOPATH/src/code.cloudfoundry.org/eirini"
-readonly GOLANGCI_LINT_VERSION="v1.7.2"
+golangci-lint --version
 
-main() {
-    lint_tool=$(getLintTool)
-    cd "$PROJECT_DIR"
-    runStaticCodeChecks "$lint_tool"
-}
-
-getLintTool() {
-    curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh| bash -s "$GOLANGCI_LINT_VERSION"
-    readlink -f bin/golangci-lint
-}
-
-runStaticCodeChecks() {
-    local lint_tool=$1
-    test -f .golangci.yml
-    $lint_tool run
-}
-
-main
+cd "$GOPATH/src/code.cloudfoundry.org/eirini"
+test -f .golangci.yml
+golangci-lint run --verbose
