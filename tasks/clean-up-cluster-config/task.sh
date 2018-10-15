cb55d17c77fadd0b39fea9f79becb9c614c612f4
Mario Nitchev <marionitchev@gmail.com>
2018-10-15 15:27:15 +0300

    Delete cluster info from private repo
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

---
+++ tasks/clean-up-cluster-config/task.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -xeuo pipefail
+IFS=$'\n\t'
+
+readonly CLUSTER_DIR="environments/kube-clusters/$CLUSTER_NAME"
+
+main() {
+    remove-scf-values
+    copy-output
+}
+
+remove-scf-values() {
+  pushd state
+    rm -r "$CLUSTER_DIR"
+  popd
+}
+
+copy-output() {
+    pushd state || exit
+        if git status --porcelain | grep .; then
+            echo "Repo is dirty"
+            git add "$CLUSTER_DIR"
+            git config --global user.email "eirini@cloudfoundry.org"
+            git config --global user.name "Come-On Eirini"
+            git commit --all --message "Delete scf values file for cluster: $CLUSTER_NAME"
+        else
+            echo "Repo is clean"
+        fi
+    popd || exit
+
+    cp -r state/. state-modified/
+}
+
+main
