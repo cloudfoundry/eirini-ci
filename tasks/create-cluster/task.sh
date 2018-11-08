19ff23b2f81173877c0151cc21aa66fb6f35ae44
Mario Nitchev <marionitchev@gmail.com>
2018-11-08 10:54:43 +0100

    move default worker count to job script
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/create-cluster/task.sh
+++ tasks/create-cluster/task.sh
@@ -9,7 +9,7 @@ source ci-resources/scripts/ibmcloud-functions
 ibmcloud-login
 
 name=${CLUSTER_NAME:?}
-worker_count=${WORKER_COUNT:?}
+worker_count=${WORKER_COUNT:-1}
 create-cluster "$name" "$worker_count"
 wait-for-state "$name" normal
 cluster-state "$name"
