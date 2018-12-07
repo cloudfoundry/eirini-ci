bec2573145b08eed4bdbb4de1beb7ae1c23c702d
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-12-07 09:39:22 +0100

    curl bits registry

--- tasks/eirini-smoke-tests/task.sh
+++ tasks/eirini-smoke-tests/task.sh
@@ -6,6 +6,7 @@ IFS=$'\n\t'
 readonly CONFIG_FILE="state/environments/kube-clusters/$CLUSTER_NAME/scf-config-values.yaml"
 
 main(){
+  INGRESS_ENDPOINT="$(goml get -f "$CONFIG_FILE" -p "opi.ingress_endpoint")"
   CF_DOMAIN="$(goml get -f "$CONFIG_FILE" -p "env.DOMAIN")"
   sleep 10
 
@@ -14,6 +15,7 @@ main(){
 
 curl-eirini-endpoints(){
   curl "api.$CF_DOMAIN/v2/info" --fail
+  curl "http://registry.$INGRESS_ENDPOINT/v2" --fail
 }
 
 main
