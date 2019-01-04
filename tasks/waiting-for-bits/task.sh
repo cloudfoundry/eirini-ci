d72db2d8083d72dfca6baef7d704cafa52464aef
Georgi Dankov <gddankov@gmail.com>
2019-01-04 14:17:42 +0200

    Wait for Bits-Service to become running
    [#162663885]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- tasks/waiting-for-bits/task.sh
+++ tasks/waiting-for-bits/task.sh
@@ -13,8 +13,16 @@ main() {
   ibmcloud-login
   export-kubeconfig "${CLUSTER_NAME:?Cluster name not provided}"
 
-  local ready
-  ready=$(is-labeled-container-ready scf "name=bits")
+  local -r expected_image_digest="$(<bits-latest/digest)"
+
+  if actual-digest-equals "name=bits" "$expected_image_digest"; then
+    echo Expected digest "$expected_image_digest" is running
+  else
+    echo Expected digest "$expected_image_digest" not running
+    exit 1
+  fi
+
+  local -r ready=$(is-labeled-container-ready scf "name=bits")
 
   if [ "$ready" = "true" ]; then
     echo Bits-Service is ready
@@ -23,6 +31,25 @@ main() {
     echo Bits-Service is NOT ready
     exit 1
   fi
+}
+
+actual-digest-equals() {
+  local -r label="${1?Label not provided}"
+  local -r expected_image_digest="${2?Expected digest not provided}"
+
+  local -r digest_len=${#expected_image_digest}
+  local -r running_image_digest="$(running-image-digest "$label" "$digest_len")"
+
+  [ "$expected_image_digest" == "$running_image_digest" ]
+}
+
+running-image-digest() {
+  local -r label="${1?Label not provided}"
+  local -r digest_len="${2?Expected digest length not provided}"
+
+  local -r running_image_id="$(kubectl get pods --selector "$label" --namespace scf --output jsonpath='{.items[].status.containerStatuses[0].imageID}')"
+
+  echo "${running_image_id:(-$digest_len)}"
 }
 
 main
