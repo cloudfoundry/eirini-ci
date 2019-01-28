ab2c735b53c332a590f5379545808c272a1a14d7
Mario Nitchev <mario.nitchev@sap.com>
2019-01-28 16:29:17 +0100

    Tagging released Eirini images as latest
    [#163519000]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- tasks/retag-release-images/task.sh
+++ tasks/retag-release-images/task.sh
@@ -7,41 +7,39 @@ IFS=$'\n\t'
 source ci-resources/scripts/docker
 readonly PIPELINE_VERSION=$(cat deployment-version/version)
 readonly VERSION=$(cat release-version/version)
-readonly CI_TAG="${CLUSTER_NAME}-${PIPELINE_VERSION}"
+readonly CI_TAG="${CLUSTER_NAME:?Missing cluster name}-${PIPELINE_VERSION}"
+readonly IMAGE_NAMES=("opi" "opi-init" "recipe" "certs-copy" "certs-generate" "secret-smuggler")
+readonly DOCKER_HUB_USER=${DOCKER_HUB_USER:?Variable not set}
+readonly DOCKER_HUB_PASSWORD=${DOCKER_HUB_PASSWORD:?Variable not set}
 
 main() {
   start-docker
   download-images
-  retag-images
-  push
+  retag-images "$VERSION"
+  retag-images latest
+  push "$VERSION"
+  push latest
 }
 
 download-images() {
-  docker pull "eirini/opi:$CI_TAG"
-  docker pull "eirini/opi-init:$CI_TAG"
-  docker pull "eirini/recipe:$CI_TAG"
-  docker pull "eirini/certs-copy:$CI_TAG"
-  docker pull "eirini/certs-generate:$CI_TAG"
-  docker pull "eirini/secret-smuggler:$CI_TAG"
+  for image_name in "${IMAGE_NAMES[@]}"; do
+    docker pull "eirini/$image_name:$CI_TAG"
+  done
 }
 
 retag-images() {
-  docker tag "eirini/opi:$CI_TAG" "eirini/opi:$VERSION"
-  docker tag "eirini/opi-init:$CI_TAG" "eirini/opi-init:$VERSION"
-  docker tag "eirini/recipe:$CI_TAG" "eirini/recipe:$VERSION"
-  docker tag "eirini/certs-copy:$CI_TAG" "eirini/certs-copy:$VERSION"
-  docker tag "eirini/certs-generate:$CI_TAG" "eirini/certs-generate:$VERSION"
-  docker tag "eirini/secret-smuggler:$CI_TAG" "eirini/secret-smuggler:$VERSION"
+  local -r version="${1:?Missing version parameter}"
+  for image_name in "${IMAGE_NAMES[@]}"; do
+    docker tag "eirini/$image_name:$CI_TAG" "eirini/$image_name:$version"
+  done
 }
 
 push() {
+  local -r version="${1:?Missing version parameter}"
   docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
-  docker push "eirini/opi:$VERSION"
-  docker push "eirini/opi-init:$VERSION"
-  docker push "eirini/recipe:$VERSION"
-  docker push "eirini/certs-copy:$VERSION"
-  docker push "eirini/certs-generate:$VERSION"
-  docker push "eirini/secret-smuggler:$VERSION"
+  for image_name in "${IMAGE_NAMES[@]}"; do
+    docker push "eirini/$image_name:$version"
+  done
 }
 
 main
