cb95fdada5fd1902168dc75d53a8b74f484b731e
Mario Nitchev <marionitchev@gmail.com>
2019-02-06 15:22:18 +0100

    Move publish-release job to acceptance. Remove automatic triggers
    [#163480419]
    
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/create-release-images/task.sh
+++ tasks/create-release-images/task.sh
@@ -5,32 +5,37 @@ IFS=$'\n\t'
 
 # shellcheck disable=SC1091
 source ci-resources/scripts/docker
-readonly PIPELINE_VERSION=$(cat deployment-version/version)
 readonly VERSION=$(cat release-version/version)
-readonly CI_TAG="${CLUSTER_NAME:?Missing cluster name}-${PIPELINE_VERSION}"
 readonly IMAGE_NAMES=("opi" "opi-init" "recipe" "certs-copy" "certs-generate" "secret-smuggler")
 readonly DOCKER_HUB_USER=${DOCKER_HUB_USER:?Variable not set}
 readonly DOCKER_HUB_PASSWORD=${DOCKER_HUB_PASSWORD:?Variable not set}
 
+export-gopath() {
+  pushd eirini-release || exit
+  # shellcheck disable=SC1091
+  source .envrc
+  popd || exit
+}
+
 main() {
   start-docker
-  download-images
-  retag-images "$VERSION"
-  retag-images latest
+  export-gopath
+  build-images
+  retag-images
   push "$VERSION"
   push latest
 }
 
-download-images() {
-  for image_name in "${IMAGE_NAMES[@]}"; do
-    docker pull "eirini/$image_name:$CI_TAG"
-  done
+build-images() {
+  pushd "eirini-release/"
+    ./docker/generate-docker-image.sh "$VERSION"
+    ./src/code.cloudfoundry.org/eirini/recipe/bin/build.sh "$VERSION"
+  popd
 }
 
 retag-images() {
-  local -r version="${1:?Missing version parameter}"
   for image_name in "${IMAGE_NAMES[@]}"; do
-    docker tag "eirini/$image_name:$CI_TAG" "eirini/$image_name:$version"
+    docker tag "eirini/$image_name:$VERSION" "eirini/$image_name:latest"
   done
 }
 
