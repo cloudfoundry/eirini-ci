8b3307b6ba5b9a17411f3bdce5579f37258e2559
Oleksandr Slynko <oslynko@pivotal.io>
2019-03-08 14:34:18 +0000

    Quote subshell to satisfy shellcheck
    [#164492200]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/helm-install/task.sh
+++ tasks/helm-install/task.sh
@@ -25,8 +25,8 @@ export-certs() {
   if [ "$COMPONENT" == "scf" ]; then
     SECRET=$(kubectl get pods --namespace uaa --output jsonpath='{.items[*].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
     CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa --output jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
-    BITS_TLS_CRT="$(kubectl get secret $(kubectl config current-context) --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
-    BITS_TLS_KEY="$(kubectl get secret $(kubectl config current-context) --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
+    BITS_TLS_CRT="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    BITS_TLS_KEY="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
   fi
 }
 
