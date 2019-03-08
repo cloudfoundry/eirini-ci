96dcc6688bd7795c31740c09f0512903ebf6c5cd
Oleksandr Slynko <oslynko@pivotal.io>
2019-03-08 14:28:55 +0000

    Pass certificates for bits service
    [#164492200]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/helm-install/task.sh
+++ tasks/helm-install/task.sh
@@ -10,19 +10,23 @@ readonly ENVIRONMENT="state/environments/kube-clusters/$CLUSTER_NAME"
 readonly VERSION="$(cat deployment-version/version)"
 export SECRET=""
 export CA_CERT=""
+export BITS_TLS_CRT=""
+export BITS_TLS_KEY=""
 
 main() {
   ibmcloud-login
   export-kubeconfig "$CLUSTER_NAME"
-  export-ca-cert
+  export-certs
   helm-dep-update
   helm-install
 }
 
-export-ca-cert() {
+export-certs() {
   if [ "$COMPONENT" == "scf" ]; then
     SECRET=$(kubectl get pods --namespace uaa --output jsonpath='{.items[*].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
     CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa --output jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
+    BITS_TLS_CRT="$(kubectl get secret $(kubectl config current-context) --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    BITS_TLS_KEY="$(kubectl get secret $(kubectl config current-context) --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
   fi
 }
 
@@ -45,6 +49,8 @@ helm-install() {
     --set "secrets.UAA_CA_CERT=${CA_CERT}" \
     --set "opi.version=$VERSION" \
     --set "eirini.opi.image_tag=$image_tag" \
+    --set "eirini.secrets.BITS_TLS_CRT=${BITS_TLS_CRT}" \
+    --set "eirini.secrets.BITS_TLS_KEY=${BITS_TLS_KEY}" \
     --force
   popd
 }
