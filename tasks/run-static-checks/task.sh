2c8300e0e5d7dfdc7fe11d62cf18a42c4fd998d9
Oleksandr Slynko <oslynko@pivotal.io>
2019-05-08 15:25:18 +0100

    Where we're going, we don't need GOPATH

--- tasks/run-static-checks/task.sh
+++ tasks/run-static-checks/task.sh
@@ -3,9 +3,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-GOPATH=$(readlink -f eirini-source)
 golangci-lint --version
 
-cd "$GOPATH/src/code.cloudfoundry.org/eirini"
+cd "eirini-source/src/code.cloudfoundry.org/eirini"
 test -f .golangci.yml
 golangci-lint run --verbose
