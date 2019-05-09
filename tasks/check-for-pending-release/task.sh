e9d179252d707aba5e23160ed8e3896460ff8d0c
Mario Nitchev <marionitchev@gmail.com>
2019-05-09 17:34:28 +0300

    Fix shfmt issue
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- tasks/check-for-pending-release/task.sh
+++ tasks/check-for-pending-release/task.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-
 cd gh-pages-pr
-open_prs="$(hub pr list --base gh-pages)" 
+open_prs="$(hub pr list --base gh-pages)"
 if [[ ! -z "$open_prs" ]]; then
   echo "Please close the following PRs befoure trying to create a new release:"
   echo "$open_prs"
   exit 1
 fi
-
