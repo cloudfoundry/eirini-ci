dbd4b6a58ee1a780a88f2849da058d073e527e5d
Julian Skupnjak <skupnjak@de.ibm.com>
2019-05-09 17:35:56 +0300

    Fix check shell issue
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/check-for-pending-release/task.sh
+++ tasks/check-for-pending-release/task.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd gh-pages-pr
 open_prs="$(hub pr list --base gh-pages)"
-if [[ ! -z "$open_prs" ]]; then
+if [[ -n "$open_prs" ]]; then
   echo "Please close the following PRs befoure trying to create a new release:"
   echo "$open_prs"
   exit 1
