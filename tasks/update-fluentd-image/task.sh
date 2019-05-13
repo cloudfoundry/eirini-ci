366b24614914e55b1765fd46f32e2c3f5f49bbac
Julian Skupnjak <skupnjak@de.ibm.com>
2019-05-13 16:27:02 +0200

    change github urls to use https
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- tasks/update-fluentd-image/task.sh
+++ tasks/update-fluentd-image/task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euox pipefail
 IFS=$'\n\t'
 
 # shellcheck disable=SC1091
@@ -13,12 +13,14 @@ main() {
 }
 
 generate-fluentd-image() {
-  pushd eirini/fluentd || exit
+  pushd eirini/fluentd || exit 1
     docker build . -t eirini/fluentd:"$TAG"
-  popd || exit
+  popd || exit 1
 }
 
 push() {
   docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
   docker push "eirini/fluentd:$TAG"
 }
+
+main
