274d75303c8ceeb47b6eb0a67a2976af87d461d4
Oleksandr Slynko <oslynko@pivotal.io>
2019-05-14 11:39:13 +0100

    GOPATH is no longer needed for unit tests
    [#165907392]

--- tasks/run-unit-tests/task.sh
+++ tasks/run-unit-tests/task.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-# shellcheck disable=SC2034
-GOPATH=$(readlink -f eirini-source)
-
 readonly WORKSPACE="$(readlink -f eirini-source)/src/code.cloudfoundry.org/eirini"
 "$WORKSPACE"/scripts/run_unit_tests.sh
