7c22ba933d949a07a8e71ac0478dd012a4668379
Mario Nitchev <marionitchev@gmail.com>
2019-05-27 11:33:43 +0300

    Fix shell check
    [#166148436]

--- tasks/hostpath-provisioner/task.sh
+++ tasks/hostpath-provisioner/task.sh
@@ -6,19 +6,19 @@ IFS=$'\n\t'
 # shellcheck disable=SC1091
 source ci-resources/scripts/ibmcloud-functions
 
-main(){
+main() {
   install_block_storage_prodivder
   install_hostpath_provider
 }
 
-install_block_storage_prodivder(){
+install_block_storage_prodivder() {
   helm repo add iks-charts https://icr.io/helm/iks-charts
   helm repo update
   helm install iks-charts/ibmcloud-block-storage-plugin
 }
 
 
-install_hostpath_provider(){
+install_hostpath_provider() {
   local HOSTPATH_PROVIDER_RBAC="https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/rbac.yaml"
   local HOSTPATH_PROVIDER_DEPLOYMENT="https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/deployment.yaml"
   local HOSTPATH_PROVIDER_STORAGECLASS="https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/storageclass.yaml"
