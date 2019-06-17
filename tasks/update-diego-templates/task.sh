610636986334e565a7bc055f26e9a287157dcf4c
Julian Skupnjak <skupnjak@de.ibm.com>
2019-06-17 11:34:39 +0200

    add -e flag for sed command
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- tasks/update-diego-templates/task.sh
+++ tasks/update-diego-templates/task.sh
@@ -10,7 +10,7 @@ main(){
 update-templates(){
   pushd eirini-release
     sed -i '2i\{{ if .Values.env.ENABLE_OPI_STAGING  }}' helm/cf/templates/diego-*.yaml
-    sed -i '\$a{{- end}}' helm/cf/templates/diego-*.yaml
+    sed -i -e '\$a{{- end}}' helm/cf/templates/diego-*.yaml
   popd || exit
 }
 
