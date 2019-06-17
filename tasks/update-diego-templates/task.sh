9bed217e3d85ce0bd5169c85532409dcfd2363a2
Oleksandr Slynko <oslynko@pivotal.io>
2019-06-17 11:25:55 +0200

    add update-diego-templates job to scf-build pipeline
    [#166572263]
    
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

---
+++ tasks/update-diego-templates/task.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+main(){
+  update-templates
+  commit
+}
+
+update-templates(){
+  pushd eirini-release
+    sed -i '2i\{{ if .Values.env.ENABLE_OPI_STAGING  }}' helm/cf/templates/diego-*.yaml
+    sed -i '\$a{{- end}}' helm/cf/templates/diego-*.yaml
+  popd || exit
+}
+
+commit(){
+  pushd eirini-release
+  if git status --porcelain | grep .; then
+    echo "Repo is dirty"
+    git add "helm/cf/templates/diego-*.yaml"
+    git config --global user.email "eirini@cloudfoundry.org"
+    git config --global user.name "Come-On Eirini"
+    git commit --all --message "update diego templates"
+  else
+    echo "Repo is clean"
+  fi
+  popd || exit
+
+  cp -r eirini-release/. eirini-release-modified/
+}
+
+main
+
+
