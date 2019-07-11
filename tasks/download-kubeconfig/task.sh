46dbde818218eb28e2ba4f413cbad354b6904809
Akshay Mankar <amankar@pivotal.io>
2019-07-11 16:16:28 +0100

    Make format shell happy
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- tasks/download-kubeconfig/task.sh
+++ tasks/download-kubeconfig/task.sh
@@ -9,4 +9,4 @@ source ci-resources/scripts/ibmcloud-functions
 ibmcloud-login
 export-kubeconfig "$CLUSTER_NAME"
 
-kubectl config view --flatten > kube/config
+kubectl config view --flatten >kube/config
