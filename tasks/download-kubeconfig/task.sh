c4e8cd8807224db268caeabe363fdc62c83fe46c
Oleksandr Slynko <oslynko@pivotal.io>
2019-07-11 16:12:10 +0100

    Flatten kubeconfig
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- tasks/download-kubeconfig/task.sh
+++ tasks/download-kubeconfig/task.sh
@@ -9,6 +9,4 @@ source ci-resources/scripts/ibmcloud-functions
 ibmcloud-login
 export-kubeconfig "$CLUSTER_NAME"
 
-readonly KUBE_RESOURCES=$(dirname "$KUBECONFIG")
-cp "$KUBE_RESOURCES"/*.pem kube
-cp "$KUBECONFIG" kube/config
+kubectl config view --flatten > kube/config
