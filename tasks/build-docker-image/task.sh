f316b5e3b7d98a2c154c46d877938797e2e194db
Mario Nitchev <marionitchev@gmail.com>
2019-07-15 17:44:45 +0300

    Build docker images in parallel
    [#164492513]

---
+++ tasks/build-docker-image/task.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# shellcheck disable=SC1091
+source ci-resources/scripts/docker
+readonly VERSION_FILE="deployment-version/version"
+readonly VERSION=$(cat "$VERSION_FILE")
+readonly TAG="${CLUSTER_NAME}-${VERSION}"
+
+main() {
+  start-docker
+  generate-opi-images
+  push
+}
+
+generate-opi-images() {
+  local git_sha
+  git_sha="$(git rev-parse HEAD)"
+
+  docker build "$CONTEXT_PATH" -t "$IMAGE_NAME:$TAG" -f "$DOCKERFILE_PATH" --build-arg "GIT_SHA=$git_sha"
+}
+
+push() {
+  docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
+  docker push "$IMAGE_NAME:$TAG"
+}
+
+main
