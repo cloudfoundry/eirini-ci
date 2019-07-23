70a246a3a3a954fdc1315e3cb2ba6ae4c39a2d95
Georgi Dankov <gddankov@gmail.com>
2019-07-23 12:24:17 +0300

    Remove redundant code in nuke-scf task
    [#164492513]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/nuke-scf/task.sh
+++ tasks/nuke-scf/task.sh
@@ -7,23 +7,11 @@ IFS=$'\n\t'
 source ci-resources/scripts/ibmcloud-functions
 
 readonly ENVIRONMENT="state/environments/kube-clusters/$CLUSTER_NAME"
-export SECRET=""
-export CA_CERT=""
-export BITS_TLS_CRT=""
-export BITS_TLS_KEY=""
 
 main() {
   ibmcloud-login
   export-kubeconfig "$CLUSTER_NAME"
-  export-certs
   helm-purge
-}
-
-export-certs() {
-  SECRET=$(kubectl get pods --namespace uaa --output jsonpath='{.items[*].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
-  CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa --output jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
-  BITS_TLS_CRT="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
-  BITS_TLS_KEY="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
 }
 
 helm-purge() {
