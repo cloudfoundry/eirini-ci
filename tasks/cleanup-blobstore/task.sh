5a300d1d03459efa622ecc67b08545a8fcbd9a05
Akshay Mankar <amankar@pivotal.io>
2019-08-06 11:47:50 +0100

    shfmt blobstore-cleanup/task.sh
    [#167702004]

--- tasks/cleanup-blobstore/task.sh
+++ tasks/cleanup-blobstore/task.sh
@@ -9,4 +9,4 @@ ibmcloud-login
 export-kubeconfig "$CLUSTER_NAME"
 
 kubectl exec -n scf blobstore-0 -c blobstore -- \
-        /bin/sh -c 'rm -rf /var/vcap/store/shared/cc-droplets/sh/a2/sha256:*'
+  /bin/sh -c 'rm -rf /var/vcap/store/shared/cc-droplets/sh/a2/sha256:*'
