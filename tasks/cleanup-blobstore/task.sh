5ef7fdf800ffd2b5dab13bfd410c5d3f7b9aa2d7
Akshay Mankar <amankar@pivotal.io>
2019-08-06 11:45:23 +0100

    Add task to cleanup blobstore
    [#167702004]

---
+++ tasks/cleanup-blobstore/task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# shellcheck disable=SC1091
+source ci-resources/scripts/ibmcloud-functions
+
+ibmcloud-login
+export-kubeconfig "$CLUSTER_NAME"
+
+kubectl exec -n scf blobstore-0 -c blobstore -- \
+        /bin/sh -c 'rm -rf /var/vcap/store/shared/cc-droplets/sh/a2/sha256:*'
