539bf2a7d0817e5c38d6f778e948e771f1f255b3
Georgi Dankov <gddankov@gmail.com>
2019-08-07 14:46:02 +0300

    Add commit message to image update commits
    [#167667591]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- tasks/update-image-digests/task.sh
+++ tasks/update-image-digests/task.sh
@@ -43,21 +43,31 @@ commit-message() {
   secret_smuggler_ref=$(cat ./eirini-secret-smuggler/.git/ref)
   fluentd_ref=$(cat ./eirini-fluentd/.git/ref)
 
+  local eirini_commit_msg opi_init_commit_msg secret_smuggler_commit_msg fluentd_commit_msg
+  eirini_commit_msg=$(cat ./eirini/.git/commit_message)
+  opi_init_commit_msg=$(cat ./eirini-opi-init/.git/commit_message)
+  secret_smuggler_commit_msg=$(cat ./eirini-secret-smuggler/.git/commit_message)
+  fluentd_commit_msg=$(cat ./eirini-fluentd/.git/commit_message)
+
   echo -e "Update image versions\n"
 
   for f in $(git -C eirini-release diff --name-only); do
     case $f in
       "helm/eirini/versions/opi-init")
         echo "OPI init commit SHA: $opi_init_ref"
+        echo -e "OPI init commit message: $opi_init_commit_msg\n"
         ;;
       "helm/eirini/versions/secret-smuggler")
         echo "Secret smuggler commit SHA: $secret_smuggler_ref"
+        echo -e "Secret smuggler commit message: $secret_smuggler_commit_msg\n"
         ;;
       "helm/eirini/versions/fluentd")
         echo "Fluentd commit SHA: $fluentd_ref"
+        echo -e "Fluentd commit message: $fluentd_commit_msg\n"
         ;;
       "helm/eirini/versions/opi")
         echo "Eirini commit SHA: $eirini_ref"
+        echo -e "Eirini commit message: $eirini_commit_msg\n"
         ;;
     esac
   done
