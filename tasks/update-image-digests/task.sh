9f171b0596a58a82a10e160922811f50f6d85e47
Akshay Mankar <amankar@pivotal.io>
2019-08-07 12:49:11 +0300

    Add eirini repo sha to image update commits
    [#167667591]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/update-image-digests/task.sh
+++ tasks/update-image-digests/task.sh
@@ -18,6 +18,8 @@ update-digest() {
 }
 
 commit-changes() {
+  local msg
+  msg=$(commit-message)
   pushd eirini-release
   if git status --porcelain | grep .; then
     echo "Repo is dirty"
@@ -25,13 +27,40 @@ commit-changes() {
     git --no-pager diff --staged
     git config --global user.email "eirini@cloudfoundry.org"
     git config --global user.name "Come-On Eirini"
-    git commit --all --message "Update image versions"
+    git commit --all --message "$msg"
   else
     echo "Repo is clean"
   fi
   popd || exit
 
   cp -r eirini-release/. eirini-release-updated
+}
+
+commit-message() {
+  local eirini_ref opi_init_ref secret_smuggler_ref fluentd_ref
+  eirini_ref=$(cat ./eirini/.git/ref)
+  opi_init_ref=$(cat ./eirini-opi-init/.git/ref)
+  secret_smuggler_ref=$(cat ./eirini-secret-smuggler/.git/ref)
+  fluentd_ref=$(cat ./eirini-fluentd/.git/ref)
+
+  echo -e "Update image versions\n"
+
+  for f in $(git -C eirini-release diff --name-only); do
+    case $f in
+      "helm/eirini/versions/opi-init")
+        echo "OPI init commit SHA: $opi_init_ref"
+        ;;
+      "helm/eirini/versions/secret-smuggler")
+        echo "Secret smuggler commit SHA: $secret_smuggler_ref"
+        ;;
+      "helm/eirini/versions/fluentd")
+        echo "Fluentd commit SHA: $fluentd_ref"
+        ;;
+      "helm/eirini/versions/opi")
+        echo "Eirini commit SHA: $eirini_ref"
+        ;;
+    esac
+  done
 }
 
 main
