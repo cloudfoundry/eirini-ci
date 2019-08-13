4a6dd5dd8be3260d6a57767df2a836835d5d0fe8
Akshay Mankar <amankar@pivotal.io>
2019-08-13 17:50:36 +0300

    Fix shfmt linter issue
    [#167322148]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/harden-cluster-security/task.sh
+++ tasks/harden-cluster-security/task.sh
@@ -11,9 +11,9 @@ export-kubeconfig "$CLUSTER_NAME"
 readonly jq_cmd='.subjects |= map(select(.name != "system:serviceaccounts" and .name != "system:authenticated" ))'
 
 kubectl get clusterrolebindings privileged-psp-user -o json |
-  jq  "$jq_cmd" |
+  jq "$jq_cmd" |
   kubectl apply -f -
 
 kubectl get clusterrolebindings restricted-psp-user -o json |
-    jq  "$jq_cmd" |
-    kubectl apply -f -
+  jq "$jq_cmd" |
+  kubectl apply -f -
