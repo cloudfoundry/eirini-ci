a4b995e4de93d88039fccc276efa614d7b2dc891
Oleksandr Slynko <oslynko@pivotal.io>
2019-08-22 14:17:18 +0100

    Rename input name to be more generic
    [#168052359]
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- tasks/make-docker-build-args/task.sh
+++ tasks/make-docker-build-args/task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-revision=$(git -C eirini rev-parse HEAD)
+revision=$(git -C repository rev-parse HEAD)
 echo "{\"GIT_SHA\": \"$revision\"}" >docker-build-args/args.json
