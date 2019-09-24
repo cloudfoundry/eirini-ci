211ff6c78285e32e4a4d6fbdd278e82905a50335
Mario Nitchev <marionitchev@gmail.com>
2019-09-24 15:26:36 +0300

    Use Google Application Default Credentials
    [#166876339]
    
    Signed-off-by: Michael Schaefer <schaefer.michael@de.ibm.com>

--- tasks/gcp-download-kubeconfig/task.sh
+++ tasks/gcp-download-kubeconfig/task.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 IFS=$'\n\t'
 echo "$GCP_SERVICE_ACCOUNT_JSON" >service-account.json
 gcloud auth activate-service-account --key-file="$PWD/service-account.json"
+gcloud config set container/use_application_default_credentials true
 
 export KUBECONFIG=kube/config
 gcloud beta container clusters get-credentials "$CLUSTER_NAME" --region europe-west1
