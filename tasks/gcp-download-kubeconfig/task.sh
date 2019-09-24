1f703c0d77bc16681a7ccab6a0dc0aaf98f8ce4d
Michael Schaefer <schaefer.michael@de.ibm.com>
2019-09-24 15:16:18 +0300

    Authenticate with service account
    [#166876339]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- tasks/gcp-download-kubeconfig/task.sh
+++ tasks/gcp-download-kubeconfig/task.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 IFS=$'\n\t'
 echo "$GCP_SERVICE_ACCOUNT_JSON" >service-account.json
-export GOOGLE_APPLICATION_CREDENTIALS="$PWD/service-account.json"
+gcloud auth activate-service-account --key-file="$PWD/service-account.json"
 
 export KUBECONFIG=kube/config
 gcloud beta container clusters get-credentials "$CLUSTER_NAME" --region europe-west1
