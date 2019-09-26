353e9234e202a7bc1962ae500b62d1d9b760270e
Oleksandr Slynko <oslynko@pivotal.io>
2019-09-26 10:43:22 +0100

    Export kubeconfig in proper location
    [#166876339]

--- tasks/gcp-download-kubeconfig/task.sh
+++ tasks/gcp-download-kubeconfig/task.sh
@@ -6,5 +6,5 @@ echo "$GCP_SERVICE_ACCOUNT_JSON" >kube/service-account.json
 gcloud auth activate-service-account --key-file="kube/service-account.json"
 gcloud config set container/use_application_default_credentials true
 
-export KUBECONFIG=config
+export KUBECONFIG=kube/config
 gcloud beta container clusters get-credentials "$CLUSTER_NAME" --region europe-west1
