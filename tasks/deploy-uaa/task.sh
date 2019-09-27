5499e6a6e81e5d5733b65d67132433e89a556fe4
Oleksandr Slynko <oslynko@pivotal.io>
2019-09-27 12:45:06 +0100

    Use cert manager certificate to deploy UAA
    [#166876339]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/deploy-uaa/task.sh
+++ tasks/deploy-uaa/task.sh
@@ -13,11 +13,22 @@ main() {
 }
 
 helm-install() {
+  local cert_args
+  if [ "$USE_CERT_MANAGER" == "true" ]; then
+    UAA_TLS_CRT="$(kubectl get secret uaa-ingress --namespace cert-manager -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    UAA_TLS_KEY="$(kubectl get secret uaa-ingress --namespace cert-manager -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
+    cert_args=(
+      "--set" "ingress.tls.crt=${UAA_TLS_CRT}"
+      "--set" "ingress.tls.key=${UAA_TLS_KEY}"
+    )
+  fi
   pushd eirini-release/helm
   helm upgrade --install "uaa" \
     "uaa" \
     --namespace "uaa" \
+    "${cert_args[@]}" \
     --values "../../$ENVIRONMENT"/scf-config-values.yaml
+
   popd
 }
 
