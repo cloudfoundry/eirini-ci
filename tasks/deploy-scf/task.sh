b4ffdc0d88e74988842b485d01f448a63a08453b
Oleksandr Slynko <oslynko@pivotal.io>
2019-10-02 16:51:37 +0100

    Use a separate certificate for bits
    [#166876339]

--- tasks/deploy-scf/task.sh
+++ tasks/deploy-scf/task.sh
@@ -20,8 +20,8 @@ main() {
 
 export-certs() {
   if [ "$USE_CERT_MANAGER" == "true" ]; then
-    BITS_TLS_CRT="$(kubectl get secret router-ingress --namespace cert-manager -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
-    BITS_TLS_KEY="$(kubectl get secret router-ingress --namespace cert-manager -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
+    BITS_TLS_CRT="$(kubectl get secret bits-ingress --namespace cert-manager -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    BITS_TLS_KEY="$(kubectl get secret bits-ingress --namespace cert-manager -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
     ROOT_CA="$(curl -s https://letsencrypt.org/certs/isrgrootx1.pem.txt)"
     INTERMEDIATE_CA="$(curl -s https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt)"
     CA_CERT="${ROOT_CA}
@@ -61,9 +61,12 @@ helm-install() {
   fi
 
   if [ "$USE_CERT_MANAGER" == "true" ]; then
+    local ingress_crt ingress_key
+    ingress_crt="$(kubectl get secret router-ingress --namespace cert-manager -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    ingress_key="$(kubectl get secret router-ingress --namespace cert-manager -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
     cert_args=(
-      "--set" "ingress.tls.crt=${BITS_TLS_CRT}"
-      "--set" "ingress.tls.key=${BITS_TLS_KEY}"
+      "--set" "ingress.tls.crt=${ingress_crt}"
+      "--set" "ingress.tls.key=${ingress_key}"
     )
   fi
 
