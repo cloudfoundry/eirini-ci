22731c0db9d6bd2a1ba980dc667045d764e2938f
Georgi Sabev <georgethebeatle@gmail.com>
2019-11-05 15:06:38 +0000

    Get cluster name from ENV instead of kubeconfig
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- tasks/deploy-scf/task.sh
+++ tasks/deploy-scf/task.sh
@@ -26,8 +26,8 @@ export-certs() {
 
 ${INTERMEDIATE_CA}"
   else
-    BITS_TLS_CRT="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
-    BITS_TLS_KEY="$(kubectl get secret "$(kubectl config current-context)" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
+    BITS_TLS_CRT="$(kubectl get secret "$CLUSTER_NAME" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)"
+    BITS_TLS_KEY="$(kubectl get secret "$CLUSTER_NAME" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)"
     SECRET=$(kubectl get pods --namespace uaa --output jsonpath='{.items[*].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
     CA_CERT="$(kubectl get secret "$SECRET" --namespace uaa --output jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)"
   fi
