788745182302864177d9fd29d2c34173d3fe6a3a
Georgi Sabev <georgethebeatle@gmail.com>
2019-11-12 12:48:06 +0200

    Add check for leftover pods after CATS
    [#169600816]
    
    Co-authored-by: Georgi Dankov <gddankov@gmail.com>

---
+++ tasks/check-leftover-pods/task.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
+export KUBECONFIG="$PWD/kube/config"
+
+pods=$(kubectl -n eirini get pods)
+if [ "$pods" != "" ]; then
+  echo "There are leftover pods in the eirini namespace:"
+  echo "$pods"
+  exit 1
+fi
