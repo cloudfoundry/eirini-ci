993a7214066c4bbae796b61f277917d0cee80f86
Akshay Mankar <amankar@pivotal.io>
2019-11-15 14:53:01 +0000

    Check only non 'Terminating' leftover pods
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- tasks/check-leftover-pods/task.sh
+++ tasks/check-leftover-pods/task.sh
@@ -3,7 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 export KUBECONFIG="$PWD/kube/config"
 
-pods=$(kubectl -n eirini get pods)
+pods=$(kubectl -n eirini get pods --field-selector='status.phase!=Terminating')
 if [ "$pods" != "" ]; then
   echo "There are leftover pods in the eirini namespace:"
   echo "$pods"
