c7d91e4bcd2187f3c79420bb474ac6ba243af802
Georgi Dankov <gddankov@gmail.com>
2019-11-20 12:46:51 +0200

    Properly ignore terminating pods
    This happens in check-leftover-pods task.
    
    [#169835758]

--- tasks/check-leftover-pods/task.sh
+++ tasks/check-leftover-pods/task.sh
@@ -3,7 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 export KUBECONFIG="$PWD/kube/config"
 
-pods=$(kubectl -n eirini get pods --field-selector='status.phase!=Terminating')
+pods=$(kubectl -n eirini get pods --no-headers=true | grep --invert-match Terminating)
 if [ "$pods" != "" ]; then
   echo "There are leftover pods in the eirini namespace:"
   echo "$pods"
