d910ffa729687034414a6e3a6ebc703e056aaf11
Georgi Dankov <gddankov@gmail.com>
2019-11-22 11:24:47 +0200

    Refer to the new terraform variable names
    [#169651132]

--- tasks/gcp-delete-cluster/task.sh
+++ tasks/gcp-delete-cluster/task.sh
@@ -6,6 +6,6 @@ echo "$GCP_SERVICE_ACCOUNT_JSON" >service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/service-account.json"
 terraform init -backend-config="prefix=terraform/state/$CLUSTER_NAME" ci-resources/gke-cluster
 terraform destroy -var "name=$CLUSTER_NAME" \
-  -var "node_count_per_zone=$WORKER_COUNT" \
+  -var "node-count-per-zone=$WORKER_COUNT" \
   -auto-approve \
   ci-resources/gke-cluster
