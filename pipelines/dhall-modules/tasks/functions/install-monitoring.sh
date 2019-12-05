855ed7dc1443c2a5f6a2b333b63808955b00822b
Georgi Sabev <georgethebeatle@gmail.com>
2019-12-05 14:50:04 +0200

    Add separate grafana dashboard for pre 1.16 k8s
    [#169914029]

--- pipelines/dhall-modules/tasks/functions/install-monitoring.sh
+++ pipelines/dhall-modules/tasks/functions/install-monitoring.sh
@@ -9,10 +9,18 @@ install_monitoring() {
   local admin_password="$2"
   local provider_specific_values="$3"
 
-  local root_url host secret_name
+  local root_url host secret_name minor_server_version dashboard_config
   host="grafana.$(cat ingress/endpoint)"
   root_url="https://$host"
   secret_name="$(cat secret/name)"
+
+  # Kubernetes < 1.16 will send metrics with old names to prometheus
+  # See https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.16.md#removed-metrics
+  minor_server_version="$(kubectl version --output json | jq -r .serverVersion.minor | sed 's/[^0-9]*//g')"
+  dashboard_config="$config_dir/cluster-overview.json"
+  if [ "$minor_server_version" -lt "16" ]; then
+    dashboard_config="$config_dir/cluster-overview-pre-1.16.json"
+  fi
 
   helm init --client-only
 
@@ -25,7 +33,7 @@ install_monitoring() {
     --namespace="$NAMESPACE" \
     --values="$config_dir/grafana-values.yml" \
     --values="$config_dir/$provider_specific_values" \
-    --set-file "dashboards.default.cluster-overview.json=$config_dir/cluster-overview.json" \
+    --set-file "dashboards.default.cluster-overview.json=$dashboard_config" \
     --set adminPassword="$admin_password" \
     --set "grafana\.ini.server.root_url=$root_url" \
     --set "ingress.hosts={$host}" \
