e91b07d633d34f9ba0fc7b2542ceb3deecb082cd
Akshay Mankar <amankar@pivotal.io>
2019-12-05 12:04:06 +0000

    Fix shellcheck
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- pipelines/dhall-modules/tasks/functions/copy-iks-cert-secret.sh
+++ pipelines/dhall-modules/tasks/functions/copy-iks-cert-secret.sh
@@ -7,7 +7,7 @@ copy_secret() {
   cluster_name="$1"
 
   ibmcloud-login
-  export-kubeconfig ${cluster_name}
+  export-kubeconfig "${cluster_name}"
 
   secret_name="$(ibmcloud ks cluster-get "${cluster_name}" --json | jq '.ingressSecretName' -r)"
   kubectl get secret "$secret_name" --namespace=default --export -o yaml |
