ba0b4cdc479c2628ee949856ecc417c8a2fa60e0
Georgi Sabev <georgethebeatle@gmail.com>
2019-12-09 16:10:17 +0200

    Make sure monitoring namespace exists
    [#169914029]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

--- pipelines/dhall-modules/tasks/functions/copy-iks-cert-secret.sh
+++ pipelines/dhall-modules/tasks/functions/copy-iks-cert-secret.sh
@@ -10,7 +10,8 @@ copy_secret() {
   export-kubeconfig "${cluster_name}"
 
   secret_name="$(ibmcloud ks cluster-get "${cluster_name}" --json | jq '.ingressSecretName' -r)"
+  kubectl get namespace monitoring || kubectl create namespace monitoring
   kubectl get secret "$secret_name" --namespace=default --export -o yaml |
-    kubectl apply --namespace="monitoring" -f - >/dev/null 2>&1
+    kubectl apply --namespace="monitoring" -f -
   echo "$secret_name" >secret/name
 }
