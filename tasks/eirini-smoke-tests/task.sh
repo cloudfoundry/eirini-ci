cb5cc30c4b8bb5a67e5ba0b810fa09330608cebf
Georgi Dankov <gddankov@gmail.com>
2019-12-11 17:19:48 +0200

    Make eirini smoke check more verbose
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- tasks/eirini-smoke-tests/task.sh
+++ tasks/eirini-smoke-tests/task.sh
@@ -38,8 +38,9 @@ check-scf-readiness() {
 
 check_api_connection() {
   local counter=0
-  while ! curl -k "https://api.$1/v2/info" --fail; do
-    echo "Unable to connect to cf api"
+  local cf_api_url="https://api.$1/v2/info"
+  while ! curl -k "$cf_api_url" --fail; do
+    echo "Unable to connect to cf api: $cf_api_url"
     if [[ $counter -gt 300 ]]; then
       echo "SCF is NOT ready" >&2
       exit 1
