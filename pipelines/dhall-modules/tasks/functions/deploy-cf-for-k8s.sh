9e3f2ea53075b7b53f05eb8836ec8b235cd63845
Oleksandr Slynko <oslynko@pivotal.io>
2020-02-10 16:29:44 +0000

    Fix shell format
    [#170313550]
    
    Signed-off-by: Julz Skupnjak <skupnjak@de.ibm.com>

--- pipelines/dhall-modules/tasks/functions/deploy-cf-for-k8s.sh
+++ pipelines/dhall-modules/tasks/functions/deploy-cf-for-k8s.sh
@@ -5,8 +5,8 @@ export KUBECONFIG="$PWD/kube/config"
 
 deploy-cf() {
   kapp deploy -a cf -f <(
-  ytt -f "cf-for-k8s/config"\
-    -f ci-resources/cf-for-k8s/istio-lb.yml \
-    -f cluster-state/environments/kube-clusters/"${1}"/scf-config-values.yaml
-      ) -y
+    ytt -f "cf-for-k8s/config" \
+      -f ci-resources/cf-for-k8s/istio-lb.yml \
+      -f cluster-state/environments/kube-clusters/"${1}"/scf-config-values.yaml
+  ) -y
 }
