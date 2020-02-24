08c3b18eb7c42b982fdb586266d1d698c1c953cf
Georgi Sabev <georgethebeatle@gmail.com>
2020-02-24 12:40:43 +0200

    Use generated values files when deploying cf4k8s
    [#170313550]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- pipelines/dhall-modules/tasks/functions/deploy-cf-for-k8s.sh
+++ pipelines/dhall-modules/tasks/functions/deploy-cf-for-k8s.sh
@@ -7,6 +7,7 @@ deploy-cf() {
   kapp deploy -a cf -f <(
     ytt -f "patched-cf-for-k8s/config" \
       -f ci-resources/cf-for-k8s/istio-lb.yml \
-      -f cluster-state/environments/kube-clusters/"${1}"/scf-config-values.yaml
+      -f cluster-state/environments/kube-clusters/"${1}"/default-values.yml \
+      -f cluster-state/environments/kube-clusters/"${1}"/loadbalancer-values.yml
   ) -y
 }
