478a88d97ba36eac7d9599d020fb56dbd3125d30
Oleksandr Slynko <oslynko@pivotal.io>
2020-03-13 17:11:43 +0000

    Fix shfmt

--- scale.sh
+++ scale.sh
@@ -4,18 +4,16 @@
 domain="scaletest.ci-envs.eirini.cf-app.com"
 cd "$1" || exit 1
 
-for i in {1..100}
-do
+for i in {1..100}; do
   app_name="${PWD##*/}-$i"
 
   cf push "$app_name" -m 512M
-  curl "https://${app_name}.${domain}" > /tmp/logs/"$app_name"
+  curl "https://${app_name}.${domain}" >/tmp/logs/"$app_name"
 done
 
-for i in {1..100}
-do
+for i in {1..100}; do
   app_name="${PWD##*/}-$i"
 
-  cf app "$app_name" >> /tmp/logs/"$app_name"
-  cf logs --recent "$app_name" >> /tmp/logs/"$app_name"
+  cf app "$app_name" >>/tmp/logs/"$app_name"
+  cf logs --recent "$app_name" >>/tmp/logs/"$app_name"
 done
