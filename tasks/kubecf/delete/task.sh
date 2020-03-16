e6e20df5de0441b8af5d85eba88f974c5beddbb7
Oleksandr Slynko <oslynko@pivotal.io>
2020-03-16 16:59:47 +0000

    Delete or skip if something does not exist

--- tasks/kubecf/delete/task.sh
+++ tasks/kubecf/delete/task.sh
@@ -4,9 +4,15 @@ set -xeuo pipefail
 
 export KUBECONFIG=kube/config
 
-helm delete --purge kubecf
-kubectl delete namespace kubecf
+helm_delete_if_exists() {
+  if ! output="$(helm del $1 --purge 2>&1)"; then
+    echo "$output" | grep --ignore-case "not found"
+  fi
+  kubectl delete namespace $1 --ignore-not-found
+}
 
-helm delete --purge cf-operator
+helm_delete_if_exists kubecf
+
+helm_delete_if_exists cf-operator
 kubectl get customresourcedefinitions,validatingwebhookconfigurations,mutatingwebhookconfigurations --output name | xargs kubectl delete
 kubectl delete namespace cf-operator
