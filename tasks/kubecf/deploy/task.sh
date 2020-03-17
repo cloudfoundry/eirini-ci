e99cdccb5715c2d03738013f03f5170df046ff58
Julian Skupnjak <skupnjak@de.ibm.com>
2020-03-17 14:52:09 +0200

    Add a privileged PSP for cf-operator
    [#171205463]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/kubecf/deploy/task.sh
+++ tasks/kubecf/deploy/task.sh
@@ -19,6 +19,10 @@ main() {
 }
 
 install-operator() {
+  kubectl delete --ignore-not-found=true psp ibm-privileged-psp
+  # add privileged psp for cf-operator
+  kubectl apply -f ci-resources/k8s-specs/cf-operator.yml
+
   helm upgrade --install cf-operator "$CF_OPERATOR_CHART_URL" \
     --namespace cf-operator \
     --set "global.operator.watchNamespace=kubecf"
