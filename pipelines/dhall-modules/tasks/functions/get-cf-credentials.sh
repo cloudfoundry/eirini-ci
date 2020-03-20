9615778943af785f5fbee082f68d8a2bcb63c230
Julian Skupnjak <skupnjak@de.ibm.com>
2020-03-20 14:50:57 +0200

    Run shfmt
    [#171205463]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- pipelines/dhall-modules/tasks/functions/get-cf-credentials.sh
+++ pipelines/dhall-modules/tasks/functions/get-cf-credentials.sh
@@ -7,7 +7,7 @@ readonly CONFIG_FILE="state/environments/kube-clusters/$CLUSTER_NAME/values.yaml
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 export KUBECONFIG="$PWD/kube/config"
 
-get-cf-credentials(){
+get-cf-credentials() {
   if helm ls --short | grep kubecf; then
     get-kubecf-credentials
   else
@@ -16,14 +16,14 @@ get-cf-credentials(){
 }
 
 get-credentials-kubecf() {
-  goml get -f "$CONFIG_FILE" -p system_domain > cf-credentials/cf-domain
+  goml get -f "$CONFIG_FILE" -p system_domain >cf-credentials/cf-domain
   kubectl get secret \
-        --namespace kubecf kubecf.var-cf-admin-password \
-        -o jsonpath='{.data.password}' \
-        | base64 --decode > cf-credentials/cf-admin-password
+    --namespace kubecf kubecf.var-cf-admin-password \
+    -o jsonpath='{.data.password}' |
+    base64 --decode >cf-credentials/cf-admin-password
 }
 
 get-credentials-scf() {
-  goml get -f "$CONFIG_FILE" -p env.DOMAIN  > cf-credentials/cf-domain
-  goml get -f "$CONFIG_FILE" -p secrets.CLUSTER_ADMIN_PASSWORD  > cf-credentials/cf-admin-password
+  goml get -f "$CONFIG_FILE" -p env.DOMAIN >cf-credentials/cf-domain
+  goml get -f "$CONFIG_FILE" -p secrets.CLUSTER_ADMIN_PASSWORD >cf-credentials/cf-admin-password
 }
