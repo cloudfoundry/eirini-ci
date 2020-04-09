0bd8da302bf5d764f22b5ce04f1ebe001245d518
Mario Nitchev <marionitchev@gmail.com>
2020-04-09 12:12:27 +0000

    Install kapp in gcloud image & fix linter issues
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/gcp-delete-cluster/task.sh
+++ tasks/gcp-delete-cluster/task.sh
@@ -6,7 +6,7 @@ echo "$GCP_SERVICE_ACCOUNT_JSON" >"$PWD/service-account.json"
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/service-account.json"
 
 if $IS_CF4K8S_DEPLOYMENT; then
-    kapp delete -a cf --yes
+  kapp delete -a cf --yes
 fi
 
 terraform init -backend-config="prefix=terraform/state/$CLUSTER_NAME" ci-resources/gke-cluster
