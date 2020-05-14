0bb1484dc677e3936c2e9edc23157d0f7fcf69a5
Julian Skupnjak <skupnjak@de.ibm.com>
2020-05-14 15:16:31 +0100

    migrate ci pipeline from dhall to aviator
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

---
+++ inline-tasks/prepare-cluster.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+IFS=$'\n\t'
+
+export KUBECONFIG="$PWD/kube/config"
+
+kubectl apply -f ci-resources/k8s-specs/tiller-service-account.yml
+kubectl apply -f ci-resources/k8s-specs/restricted-psp.yaml
+helm init --service-account tiller --upgrade
+
