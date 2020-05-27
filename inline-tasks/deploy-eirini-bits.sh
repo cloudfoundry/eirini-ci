a29f9602c2c5d06ec802a012ebc1b06242c0843a
Mario Nitchev <marionitchev@gmail.com>
2020-05-27 13:18:46 +0000

    Fix linter issue

--- inline-tasks/deploy-eirini-bits.sh
+++ inline-tasks/deploy-eirini-bits.sh
@@ -30,8 +30,8 @@ create-test-secret() {
   nats_password_b64="$(echo -n "$NATS_PASSWORD" | base64)"
 
   openssl req -nodes -new -x509 -keyout test.key -out test.cert -subj "/CN=test"
-  cert=$(cat test.cert | base64 -w0)
-  key=$(cat test.key | base64 -w0)
+  cert=$(base64 -w0 <test.cert)
+  key=$(base64 -w0 <test.key)
   rm test.*
 
   secrets_file=$(mktemp)
