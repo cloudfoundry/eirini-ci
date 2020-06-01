223d6a5617f9ce286250a278b671eba51351e2ac
Mario Nitchev <marionitchev@gmail.com>
2020-06-01 10:02:59 +0000

    Don't install nginx on gcp clusters
    [#172911087]
    
    Signed-off-by: Danail Branekov <danailster@gmail.com>

--- tasks/install-helm-dependencies/task.sh
+++ tasks/install-helm-dependencies/task.sh
@@ -3,14 +3,10 @@
 set -xeuo pipefail
 IFS=$'\n\t'
 
-# shellcheck disable=SC1091
-source ci-resources/scripts/gcloud-functions
-
 export KUBECONFIG=kube/config
 
 main() {
   helm init --client-only
-  install-nginx-chart
   install-cert-manager-chart
   create-dns-editor-secret
   create-issuer
@@ -22,24 +18,6 @@ create-dns-editor-secret() {
 
 create-issuer() {
   kubectl apply -f ci-resources/cert-manager/letsencrypt-dns-issuer.yaml
-}
-
-cert-status() {
-  local cert_name=${1:?No cert name}
-  kubectl get certificate -n cert-manager "$cert_name" -o jsonpath='{.status.conditions[?(@.type == "Ready")].status}'
-}
-
-install-nginx-chart() {
-  local static_ip
-  gcloud-login
-  static_ip="$(get-static-ip "$CLUSTER_NAME")"
-  helm upgrade nginx-ingress \
-    --namespace nginx \
-    --install \
-    --set rbac.create=true \
-    --set controller.service.loadBalancerIP="$static_ip" \
-    stable/nginx-ingress \
-    --wait
 }
 
 install-cert-manager-chart() {
