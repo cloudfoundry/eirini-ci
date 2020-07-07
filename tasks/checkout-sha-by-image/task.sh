2218f2dd569cf35a6c0c90a216491f5f783a7cb2
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-07-07 14:26:15 +0000

    Use the Docker API to extract the image revision
    Using buildah on an overlay volume doesn't work, and we don't need to
    pull the image anyway, as we only need to read its config. This pulls
    the config directly from the Docker API.
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- tasks/checkout-sha-by-image/task.sh
+++ tasks/checkout-sha-by-image/task.sh
@@ -2,10 +2,29 @@
 
 set -euo pipefail
 
+get_token() {
+  local image=$1
+
+  curl \
+    --silent \
+    "https://auth.docker.io/token?scope=repository:$image:pull&service=registry.docker.io" |
+    jq -r '.token'
+}
+
+get_image_config() {
+  local image=$1
+  local digest=$2
+  local token=$(get_token $image)
+
+  curl \
+    --silent \
+    --location \
+    --header "Authorization: Bearer $token" \
+    "https://registry-1.docker.io/v2/$image/blobs/$digest"
+}
+
 image_sha=$(cat "eirini-release/helm/eirini/versions/$VERSION_FILE")
-buildah pull "$IMAGE_NAME@$image_sha"
+repo_sha=$(get_image_config "$IMAGE_NAME" "$image_sha" | jq -r '.Labels["org.opencontainers.image.revision"]')
 
-repo_sha=$(buildah inspect "$IMAGE_NAME@$image_sha" | jq -r '.Docker.config.Labels["org.opencontainers.image.revision"]')
 git -C repository checkout "$repo_sha"
-
 git clone repository repository-modified
