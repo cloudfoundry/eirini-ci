19b3e870367baf31680aa081142b5b22743af3d4
Mario Nitchev <marionitchev@gmail.com>
2020-08-10 13:11:35 +0000

    Delete cluster even if firwall rule is not found
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- inline-tasks/gcp-delete-cluster.sh
+++ inline-tasks/gcp-delete-cluster.sh
@@ -25,7 +25,7 @@ pushd ci-resources/gke-cluster || exit 1
     gcloud-login
     firewall_rules=$(gcloud compute firewall-rules list --filter=network="$cluster_network" --format="value(name)")
     for rule_name in $firewall_rules; do
-      gcloud compute firewall-rules delete "$rule_name" --quiet
+      gcloud compute firewall-rules delete "$rule_name" --quiet || echo "firewall rule $rule_name not found"
     done
   fi
 
