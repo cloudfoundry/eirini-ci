fca8f6c9e277bbf0326c612b594e761f42ed344e
Danail Branekov <danailster@gmail.com>
2020-08-13 12:21:52 +0000

    Resolve the GOOGLE_APPLICATION_CREDENTIALS to absolute path
    ...if set. This is needed to run integration tests against GKE clusters
    
    [#173809746]
    
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- inline-tasks/run-integration-tests.sh
+++ inline-tasks/run-integration-tests.sh
@@ -3,4 +3,8 @@
 set -e
 
 readonly WORKSPACE="$(readlink -f eirini)"
+
+if [[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+  export GOOGLE_APPLICATION_CREDENTIALS=$(readlink -f $GOOGLE_APPLICATION_CREDENTIALS)
+fi
 INTEGRATION_KUBECONFIG="$PWD"/kube/config "$WORKSPACE"/scripts/run_integration_tests.sh
