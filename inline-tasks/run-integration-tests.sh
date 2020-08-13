b512846b48f6fb95696a710bbf41fc7f0a5b87e0
Georgi Sabev <georgethebeatle@gmail.com>
2020-08-13 13:01:37 +0000

    Fix shell check
    [#173809746]
    
    Signed-off-by: Danail Branekov <danailster@gmail.com>

--- inline-tasks/run-integration-tests.sh
+++ inline-tasks/run-integration-tests.sh
@@ -4,7 +4,7 @@ set -e
 
 readonly WORKSPACE="$(readlink -f eirini)"
 
-if [[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+if [[ -n $GOOGLE_APPLICATION_CREDENTIALS ]]; then
   export GOOGLE_APPLICATION_CREDENTIALS=$(readlink -f $GOOGLE_APPLICATION_CREDENTIALS)
 fi
 INTEGRATION_KUBECONFIG="$PWD"/kube/config "$WORKSPACE"/scripts/run_integration_tests.sh
