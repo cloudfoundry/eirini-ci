e824cda81f749f4a8d0867f85bd4ede0c04ed492
Georgi Dankov <gddankov@gmail.com>
2020-08-14 12:41:41 +0000

    Vanilla k8s ci works!
    [#173809746]
    
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

---
+++ inline-tasks/run-eats-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+GOOGLE_APPLICATION_CREDENTIALS="$(readlink -f $GOOGLE_APPLICATION_CREDENTIALS)"
+
+readonly WORKSPACE="$(readlink -f eirini)"
+
+INTEGRATION_KUBECONFIG="$(readlink -f $KUBECONFIG)" "$WORKSPACE"/scripts/run_eats_tests.sh
