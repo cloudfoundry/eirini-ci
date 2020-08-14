4b25017b87d0e90192185195996580ed0ba8a4f9
Georgi Sabev <georgethebeatle@gmail.com>
2020-08-14 12:48:50 +0000

    Fix shell check
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- inline-tasks/run-eats-tests.sh
+++ inline-tasks/run-eats-tests.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-GOOGLE_APPLICATION_CREDENTIALS="$(readlink -f $GOOGLE_APPLICATION_CREDENTIALS)"
+GOOGLE_APPLICATION_CREDENTIALS="$(readlink -f "$GOOGLE_APPLICATION_CREDENTIALS")"
 
 readonly WORKSPACE="$(readlink -f eirini)"
 
-INTEGRATION_KUBECONFIG="$(readlink -f $KUBECONFIG)" "$WORKSPACE"/scripts/run_eats_tests.sh
+INTEGRATION_KUBECONFIG="$(readlink -f "$KUBECONFIG")" "$WORKSPACE"/scripts/run_eats_tests.sh
