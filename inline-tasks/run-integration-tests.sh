112327635776f5351bb8ef73abf32b8091e4dff1
Danail Branekov <danailster@gmail.com>
2020-08-20 12:47:26 +0100

    Split integration and EATs tests in CI
    Only runs EATs, not integration, against ci cluster (IBM).
    Add new job after run-tests (unit tests) to run the integration tests
    against the ci cluster.
    
    Include a refactoring of create/delete-gke-cluster to use spruce
    operators to control the cluster name, with a view to including these
    jobs twice in the pipeline to create two GKE clusters.
    
    [#174120957]

--- inline-tasks/run-integration-tests.sh
+++ inline-tasks/run-integration-tests.sh
@@ -8,4 +8,4 @@ if [[ -n $GOOGLE_APPLICATION_CREDENTIALS ]]; then
   export GOOGLE_APPLICATION_CREDENTIALS
   GOOGLE_APPLICATION_CREDENTIALS=$(readlink -f "$GOOGLE_APPLICATION_CREDENTIALS")
 fi
-INTEGRATION_KUBECONFIG="$PWD"/kube/config "$WORKSPACE"/scripts/run_integration_tests.sh
+INTEGRATION_KUBECONFIG="$PWD"/kube/config "$WORKSPACE"/scripts/run_integration_tests.sh --skipPackage eats
