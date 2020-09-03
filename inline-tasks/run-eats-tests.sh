112d3dc476c583ffad275d7b7a129d5d9e2c4a1d
Mario Nitchev <marionitchev@gmail.com>
2020-09-03 12:13:08 +0100

    Export eats env variables for helmless deployment
    [#174363619]
    
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

--- inline-tasks/run-eats-tests.sh
+++ inline-tasks/run-eats-tests.sh
@@ -9,4 +9,11 @@ fi
 
 readonly WORKSPACE="$(readlink -f eirini)"
 
+if [[ -n "$HELMLESS" ]]; then
+  export EIRINI_ADDRESS EIRINI_TLS_SECRET EIRINI_SYSTEM_NS
+  EIRINI_ADDRESS="https://$(<kube/external-ip):8085"
+  EIRINI_TLS_SECRET="eirini-certs"
+  EIRINI_SYSTEM_NS="eirini-core"
+fi
+
 INTEGRATION_KUBECONFIG="$(readlink -f "$KUBECONFIG")" "$WORKSPACE"/scripts/run_eats_tests.sh
