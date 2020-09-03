19e393c4f075fb1c34a0bbe882e510542cf6025c
Georgi Sabev <georgethebeatle@gmail.com>
2020-09-03 15:04:29 +0000

    Set current-context in the kubeconfig and use privileged container
    when running telepresence.
    
    Co-authored-by: Dimitris Karakasilis <dkarakasilis@suse.com>

--- inline-tasks/download-kubeconfig.sh
+++ inline-tasks/download-kubeconfig.sh
@@ -9,3 +9,7 @@ source ci-resources/scripts/ibmcloud-functions
 ibmcloud-login
 export-kubeconfig "$CLUSTER_NAME"
 store-kubeconfig "kube/config"
+
+export KUBECONFIG="kube/config"
+context_name=$(kubectl config get-contexts -o name | head -1)
+kubectl config use-context "$context_name"
