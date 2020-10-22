aa318bb5b1b5625ece4a70150fba265a45be9cbe
Kieron Browne <kbrowne@vmware.com>
2020-10-22 17:00:41 +0100

    Remove unnecessary values manipulating and ytt overlaying
    These are already present in either eirini values or cf-4-k8s overlays.
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- inline-tasks/patch-eirini-release-cf-for-k8s.sh
+++ inline-tasks/patch-eirini-release-cf-for-k8s.sh
@@ -5,45 +5,8 @@ set -euo pipefail
 tar xzvf cf-for-k8s-github-release/source.tar.gz -C .
 sha="$(<cf-for-k8s-github-release/commit_sha)"
 src_folder="cloudfoundry-cf-for-k8s-${sha:0:7}"
+
 rm -rf "$src_folder/build/eirini/_vendir/eirini"
-
-build_path="./$src_folder/build/eirini/"
-eirini_values="$build_path/eirini-values.yml"
-eirini_custom_values="$build_path/eirini-custom-values.yml"
-eirini_values_merged="$build_path/eirini-values-merged.yml"
-
-cat >"$build_path/add-namespaces-overlay.yml" <<EOF
-#@ load("@ytt:overlay", "overlay")
-
-#@overlay/match by=overlay.subset({"kind":"Namespace", "metadata":{"name":"cf-workloads"}})
-#@overlay/remove
----
-
-EOF
-
-cat >>"$eirini_custom_values" <<EOF
----
-opi:
-  serviceName: eirini
-  lrpController:
-    tls:
-      secretName: "eirini-internal-tls-certs"
-      keyPath: "tls.key"
-      caPath: "tls.ca"
-      certPath: "tls.crt"
-  tasks:
-    tls:
-      taskReporter:
-          secretName: "eirini-internal-tls-certs"
-          keyPath: "tls.key"
-          caPath: "tls.ca"
-          certPath: "tls.crt"
-
-EOF
-
-yq -s '.[0] * .[1]' "$eirini_values" "$eirini_custom_values" -y >"$eirini_values_merged"
-mv "$eirini_values_merged" "$eirini_values"
-
 cp -r eirini-release/helm/eirini "$src_folder/build/eirini/_vendir/"
 
 ./"$src_folder"/build/eirini/build.sh
