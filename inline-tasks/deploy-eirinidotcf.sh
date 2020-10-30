adcf983b83cf8502c7378ced08b4cb1a8f578174
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-10-30 16:36:46 +0000

    Do not deploy eirinidotcf api and db
    [#175245691]
    
    Co-authored-by: Georgi Dankov <gddankov@gmail.com>

--- inline-tasks/deploy-eirinidotcf.sh
+++ inline-tasks/deploy-eirinidotcf.sh
@@ -13,36 +13,6 @@ cf create-org eirinidotcf || true
 cf create-space -o eirinidotcf eirinidotcf || true
 cf target -o eirinidotcf -s eirinidotcf
 
-export MYSQL_ROOT_PASSWORD
-export MYSQL_PORT
-export MYSQL_IP_ADDRESS
-
-MYSQL_ROOT_PASSWORD="$(<db-conf/mysql-root-password)"
-MYSQL_PORT="$(<db-conf/mysql-port)"
-MYSQL_IP_ADDRESS="$(<db-conf/mysql-ip-address)"
-
-ups="$(
-  cat <<EOF
-{
-  "db_name": "pheed",
-  "username":"root",
-  "password":"$MYSQL_ROOT_PASSWORD",
-  "db_address": "$MYSQL_IP_ADDRESS:$MYSQL_PORT"
-}
-EOF
-)"
-
-pushd eirinidotcf/api || exit 1
-cf push --no-start
-if cf service pheed-db; then
-  cf uups pheed-db -p "$ups"
-else
-  cf cups pheed-db -p "$ups"
-fi
-cf bind-service eirinidotcf-api pheed-db
-cf start eirinidotcf-api
-popd || exit 1
-
 pushd eirinidotcf/web || exit 1
 cat >src/config.json.sh <<EOF
 {
