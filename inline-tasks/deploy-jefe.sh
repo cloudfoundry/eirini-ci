b7702c3b287d08f5b1e78a8a6dbd4c1ea2ee8a44
Georgi Dankov <gddankov@gmail.com>
2020-10-30 16:37:33 +0000

    Update jefe deployment script
    [#175245691]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- inline-tasks/deploy-jefe.sh
+++ inline-tasks/deploy-jefe.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-values=eirini-private-config/environments/kube-clusters/acceptance/values.yaml
-cf_admin_password="$(goml get -f "$values" -p "secrets.CLUSTER_ADMIN_PASSWORD")"
-cf_domain="$(goml get -f "$values" -p "env.DOMAIN")"
+values=eirini-private-config/environments/kube-clusters/cf4k8s4a8e/default-values.yml
+cf_domain="$(goml get -f "$values" -p "system_domain")"
+cf_admin_password="$(goml get -f "$values" -p "cf_admin_password")"
+
 cf api "api.$cf_domain" --skip-ssl-validation
 cf auth admin "$cf_admin_password"
+
+cf create-org jefe || true
+cf create-space -o jefe jefe || true
 cf target -o jefe -s jefe
 
 set -x
