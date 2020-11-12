0e21c5a9f64b58b81dca9119ef645780c91bc0b7
Danail Branekov <danailster@gmail.com>
2020-11-12 10:37:59 +0000

    Add option to not delete cf when redeploying cf-for-k8s
    [#175245691]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- inline-tasks/delete-cf-for-k8s.sh
+++ inline-tasks/delete-cf-for-k8s.sh
@@ -3,5 +3,11 @@
 set -euo pipefail
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 export KUBECONFIG="$PWD/kube/config"
+readonly DO_NOT_DELETE_CF=${DO_NOT_DELETE_CF:-"false"}
+
+if [[ "$DO_NOT_DELETE_CF" == "true" ]]; then
+  echo "DO_NOT_DELETE_CF set to true. Skipping delete"
+  exit 0
+fi
 
 kapp delete -a cf --yes
