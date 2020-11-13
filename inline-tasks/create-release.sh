e1fd5b9ccbc214e1904ce6300f3125f37b93c5c4
Mario Nitchev <marionitchev@gmail.com>
2020-11-13 14:12:29 +0000

    Do not release SCF and UAA
    [#175245691]
    
    Co-authored-by: Dimitris Karakasilis <dkarakasilis@suse.com>

--- inline-tasks/create-release.sh
+++ inline-tasks/create-release.sh
@@ -9,9 +9,6 @@ readonly VERSION=$(cat "$VERSION_FILE")
 
 main() {
   update-chart-version
-  update-requirements-version
-  helm-dep-update
-  update-requirements-repo
   zip-templates
 }
 
@@ -19,26 +16,8 @@ update-chart-version() {
   goml set -f eirini-release/helm/eirini/Chart.yaml -p version -v "$VERSION"
 }
 
-update-requirements-version() {
-  goml set -f eirini-release/helm/cf/requirements.yaml -p dependencies.name:eirini.version -v "$VERSION"
-}
-
-helm-dep-update() {
-  pushd "eirini-release/helm/cf"
-  helm init --client-only
-  helm repo add bits https://cloudfoundry-incubator.github.io/bits-service-release/helm
-  helm dependency update
-  popd || exit
-}
-
-update-requirements-repo() {
-  goml set -f eirini-release/helm/cf/requirements.yaml -p dependencies.name:eirini.repository -v https://cloudfoundry-incubator.github.io/eirini-release
-}
-
 zip-templates() {
   pushd eirini-release/helm
-  tar -zcvf "eirini-cf.tgz" cf
-  tar -zcvf "eirini-uaa.tgz" uaa
   tar -zcvf "eirini.tgz" eirini
   popd
 
