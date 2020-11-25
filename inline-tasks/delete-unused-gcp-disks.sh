0468bbd2a52d9dcdcfb62d7e44f6633e04955e34
Mario Nitchev <marionitchev@gmail.com>
2020-11-25 16:02:38 +0000

    Add maintenance pipeline. Add job to delete unused gcp disks
    [#175893074]
    
    Co-authored-by: Dimitris Karakasilis <dkarakasilis@suse.com>

---
+++ inline-tasks/delete-unused-gcp-disks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "$GCP_SERVICE_ACCOUNT_JSON" >"$PWD/service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$PWD/service-account.json"
+
+# shellcheck disable=SC1091
+source ci-resources/scripts/gcloud-functions
+
+delete-disk() {
+  local disk_name zone
+  disk_name="$1"
+  zone="$2"
+  echo "Deleting disk: $disk_name in region: $zone"
+  gcloud compute disks delete -q "$disk_name" --zone "$zone"
+}
+
+main() {
+  gcloud-login
+  disks=$(gcloud compute disks list --filter="-users:*" --format="csv[separator=' ',no-heading](name, location())")
+  if [[ -z "$disks" ]]; then
+    echo "Nothing to delete!"
+    exit 0
+  fi
+
+  while IFS= read -r line; do
+    local disk_name zone
+    disk_name="$(echo $line | awk '{print $1}')"
+    zone="$(echo $line | awk '{print $2}')"
+    delete-disk "$disk_name" "$zone"
+  done < <(echo "${disks}")
+}
+
+main
