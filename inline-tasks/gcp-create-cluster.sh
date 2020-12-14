e815a86939bd33e87eba937faf693e3aae9e9b97
Danail Branekov <danailster@gmail.com>
2020-12-14 18:20:40 +0200

    Optionally create windows nodepools for dev envs
    [#175895408]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- inline-tasks/gcp-create-cluster.sh
+++ inline-tasks/gcp-create-cluster.sh
@@ -4,8 +4,14 @@ set -eu
 
 echo "$GCP_SERVICE_ACCOUNT_JSON" >service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/service-account.json"
-terraform init -backend-config="prefix=terraform/state/$CLUSTER_NAME" ci-resources/gke-cluster
+
+TERRAFORM_CONFIG_PATH=ci-resources/gke-cluster
+if [[ $WINDOWS_POOL == "true" ]]; then
+  TERRAFORM_CONFIG_PATH=ci-resources/gke-cluster-windows
+fi
+
+terraform init -backend-config="prefix=terraform/state/$CLUSTER_NAME" "$TERRAFORM_CONFIG_PATH"
 terraform apply -var "name=$CLUSTER_NAME" \
   -var "node-count-per-zone=$WORKER_COUNT" \
   -auto-approve \
-  ci-resources/gke-cluster
+  "$TERRAFORM_CONFIG_PATH"
