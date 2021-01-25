8aa537abd711bdf3aa41eb52785f0864c1b442e0
Georgi Sabev <georgethebeatle@gmail.com>
2021-01-25 11:03:25 +0000

    Use default values for eirini helm chart
    [#173896604]
    
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- inline-tasks/patch-eirini-release-cf-for-k8s.sh
+++ inline-tasks/patch-eirini-release-cf-for-k8s.sh
@@ -9,7 +9,7 @@ trap "rm -rf $render_dir" EXIT
 
 rm -rf "$src_folder/build/eirini/_vendir/eirini"
 
-eirini-release/scripts/render-templates.sh cf-system "$render_dir" --values eirini-release/scripts/assets/cf4k8s-value-overrides.yml
+eirini-release/scripts/render-templates.sh cf-system "$render_dir"
 mv "${render_dir}/templates" "$src_folder/build/eirini/_vendir/eirini"
 
 ./"$src_folder"/build/eirini/build.sh
