d235f65980f657e2bab1abb3347ac89e686fefd2
Danail Branekov <danailster@gmail.com>
2021-04-09 18:27:03 +0300

    Really fix the check-golang-linters task
    [#177244106]
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- inline-tasks/check-golangci-linters.sh
+++ inline-tasks/check-golangci-linters.sh
@@ -9,7 +9,9 @@ main() {
   # Catch invalid configuration or linters that no longer exist. Errors are printed to stderr
   golangci-lint linters >/dev/null
 
-  unknown_linters="$(comm -3 <(yq eval '.linters.intentionally-disabled[].name' ./.golangci.yml | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort))"
+  cd eirini
+  config_file=.golangci.yml
+  unknown_linters="$(comm -3 <(yq eval '.linters.intentionally-disabled[].name' $config_file | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort))"
   if [[ -z "$unknown_linters" ]]; then
     exit 0
   fi
@@ -17,10 +19,10 @@ main() {
   echo -e "${bold}It seems that out golangci-lint configuration needs some attention${normal}"
   echo
   echo -e "${bold}Newly introduced linters (either enabled them, or document why we do not want to have them enabled):${normal}"
-  comm -13 <(yq eval '.linters.intentionally-disabled[].name' ./.golangci.yml | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort)
+  comm -13 <(yq eval '.linters.intentionally-disabled[].name' $config_file | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort)
   echo
   echo -e "${bold}Intentionally disabled linters that no longer exist, please remove them from the intentionally disabled list:${normal}"
-  comm -23 <(yq eval '.linters.intentionally-disabled[].name' ./.golangci.yml | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort)
+  comm -23 <(yq eval '.linters.intentionally-disabled[].name' $config_file | sort) <(golangci-lint linters | awk -F ':' '{ if (disabled==1) print $1 }; /Disabled by your/ { disabled=1 }' | sort)
 
   exit 1
 }
