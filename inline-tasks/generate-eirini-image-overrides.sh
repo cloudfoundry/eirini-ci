4ae738c9b6b9803056de2cda22f0159d6d2e86b3
Georgi Sabev <georgethebeatle@gmail.com>
2021-04-14 13:54:23 +0000

    Ignore linter warning
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

--- inline-tasks/generate-eirini-image-overrides.sh
+++ inline-tasks/generate-eirini-image-overrides.sh
@@ -4,5 +4,7 @@ cat >eirini-image-overrides/values.yml <<EOF
 #@data/values
 ---
 EOF
+
+# shellcheck disable=SC2016
 yq eval 'del(.images.eirini_controller) | del(.images.resource_validator) | .images as $images | {"images": {"eirini": $images}}' \
   eirini-release/helm/values.yaml >>eirini-image-overrides/values.yml
