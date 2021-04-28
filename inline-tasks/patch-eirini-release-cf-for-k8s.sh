796ae00a7c27a045cc4a3de47dd3d4cb9fee5662
Giuseppe Capizzi <gcapizzi@pivotal.io>
2021-04-28 17:14:40 +0100

    vendir sync when deploying to acceptance
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- inline-tasks/patch-eirini-release-cf-for-k8s.sh
+++ inline-tasks/patch-eirini-release-cf-for-k8s.sh
@@ -10,7 +10,12 @@ trap "rm -rf $render_dir" EXIT
 rm -rf "$src_folder/build/eirini/_vendir/eirini"
 
 eirini-release/scripts/render-templates.sh cf-system "$render_dir" --values eirini-release/scripts/assets/cf-for-k8s-value-overrides.yml
-mv "${render_dir}/templates" "$src_folder/build/eirini/_vendir/eirini"
+mv "$render_dir/templates" "$render_dir/eirini"
+pushd "$src_folder"
+{
+  vendir sync -d "build/eirini/_vendir=$render_dir"
+}
+popd
 
 ./"$src_folder"/build/eirini/build.sh
 
