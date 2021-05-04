6351bcf4b62fbd8ed00e8e8d17f62ff15c8d41a3
Giuseppe Capizzi <gcapizzi@pivotal.io>
2021-05-04 12:12:41 +0000

    Handle Redis upgrades in the Postfacto pipeline

--- inline-tasks/helm-install-redis.sh
+++ inline-tasks/helm-install-redis.sh
@@ -5,9 +5,24 @@ set -eu
 export KUBECONFIG="$PWD/kube/config"
 export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-kubectl create namespace postfacto-redis || true
-helm upgrade postfacto-redis \
-  --install bitnami/redis \
+CHART_STATUS="$(helm status postfacto-redis \
   --namespace postfacto-redis \
-  --set securityContext.fsGroup=65531
+  --output json |
+  jq -r ".info.status")"
+
+echo "Redis chart status: $CHART_STATUS"
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+if [[ "$CHART_STATUS" == "deployed" ]]; then
+  REDIS_PASSWORD=$(kubectl get secret --namespace "postfacto-redis" postfacto-redis -o jsonpath="{.data.redis-password}" | base64 --decode)
+  helm upgrade postfacto-redis bitnami/redis \
+    --namespace postfacto-redis \
+    --set securityContext.fsGroup=65531 \
+    --set auth.password=$REDIS_PASSWORD
+else
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  helm install postfacto-redis bitnami/redis
+  --namespace postfacto-redis \
+    --create-namespace \
+    --set securityContext.fsGroup=65531
+fi
