bf319065c2565d514350dd64dd20514be9185f9d
Mario Nitchev <marionitchev@gmail.com>
2021-06-07 13:12:55 +0300

    Delete Network Endpoint Groups before destroying cluster
    they are created for the rapid channel version of kube clusters and
    terraform does not know about them

--- pipelines/shared/_ytt_lib/ci/tasks/gcp-delete-cluster.sh
+++ pipelines/shared/_ytt_lib/ci/tasks/gcp-delete-cluster.sh
@@ -17,16 +17,28 @@ pushd ci-resources/gke-cluster || exit 1
     exit 0
   fi
 
-  # Firewall rules are created when a LoadBalancer Service is created in GCP,
-  # and since terraform doesn't know about them, they have to be manually deleted.
-  # See https://github.com/terraform-providers/terraform-provider-google/issues/5948
   cluster_network="$(terraform show -json | jq -r '.values.root_module.resources[] | select(.name == "network") | .values.name')"
   if [[ -n "$cluster_network" ]]; then
     gcloud-login
+
+    # Firewall rules are created when a LoadBalancer Service is created in GCP,
+    # and since terraform doesn't know about them, they have to be manually deleted.
+    # See https://github.com/terraform-providers/terraform-provider-google/issues/5948
     firewall_rules=$(gcloud compute firewall-rules list --filter=network="$cluster_network" --format="value(name)")
     for rule_name in $firewall_rules; do
       gcloud compute firewall-rules delete "$rule_name" --quiet || echo "firewall rule $rule_name not found"
     done
+
+    # Network endpoint groups are created for the rapid chanel version of Kubernetes
+    # clusters on GCP (couldn't find a docs link). Since terraform doesn't know about
+    # these resources they also must be deleted separately.
+    network_endpoint_groups_csv=$(gcloud beta compute network-endpoint-groups list --filter="network ~ $CLUSTER_NAME" --format="csv[no-heading](name,zone)")
+    for name_zone in $network_endpoint_groups_csv; do
+      name="$(echo $name_zone | awk -F ',' '{ print $1 }')"
+      zone="$(echo $name_zone | awk -F ',' '{ print $2 }')"
+      gcloud beta compute network-endpoint-groups delete --zone "$zone" "$name" --quiet
+    done
+
   fi
 
   terraform destroy -var "name=$CLUSTER_NAME" \
