296e547366fd67acceec9e03e8db0382794742e3
Georgi Sabev <georgethebeatle@gmail.com>
2021-06-22 15:57:45 +0000

    Build and deploy eirini-controller in ci-controller pipeline
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/shared/_ytt_lib/ci/tasks/build-eirini-controller.sh
+++ pipelines/shared/_ytt_lib/ci/tasks/build-eirini-controller.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-eirini_controller_path="$(readlink -f eirini-controller)"
-readonly eirini_controller_path
+eirini_controller_built_path="$(readlink -f eirini-controller-built)"
+readonly eirini_controller_built_path
 
-cp -a "$eirini_controller_path" eirini-controller-built
-./eirini-controller-built/deployment/scripts/build.sh
+trap "pkill dockerd" EXIT
+
+start-docker &
+
+echo 'until docker info; do sleep 5; done' >/usr/local/bin/wait_for_docker
+chmod +x /usr/local/bin/wait_for_docker
+timeout 300 wait_for_docker
+
+docker <<<"$DOCKERHUB_PASS" login --username "$DOCKERHUB_USER" --password-stdin
+
+"$eirini_controller_built_path"/deployment/scripts/build.sh
