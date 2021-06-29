ff2fdaa4089260bd8b86dd359db2a1695ab5c93f
Danail Branekov <danailster@gmail.com>
2021-06-29 17:57:02 +0300

    The publish release bit of the controller pipeline

---
+++ pipelines/shared/_ytt_lib/ci/tasks/create-controller-release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+RENDER_DIR=$(mktemp -d)
+readonly RENDER_DIR
+
+# shellcheck disable=SC2064
+trap "rm -r $RENDER_DIR" EXIT
+
+main() {
+  generate-eirini-controller-yamls
+  zip-eirini-controller-yamls
+}
+
+generate-eirini-controller-yamls() {
+  local values_yaml="$PWD/state/eirini-controller/values.yaml"
+  pushd eirini-controller/deployment/helm
+  {
+    helm template eirini-controller . --namespace eirini-controller --values "$values_yaml" --output-dir=$RENDER_DIR
+  }
+  popd
+}
+
+zip-eirini-controller-yamls() {
+  pushd "$RENDER_DIR"
+  {
+    tar -zcvf "eirini-controller.tgz" eirini-controller
+  }
+  popd
+
+  mv "$RENDER_DIR/eirini-controller.tgz" release-output
+}
+
+main
