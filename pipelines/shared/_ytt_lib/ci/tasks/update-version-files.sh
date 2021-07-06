df747f57ed7140e81643c423d27ca3813c656c99
Georgi Sabev <georgethebeatle@gmail.com>
2021-07-06 14:21:04 +0000

    Fix shellcheck

--- pipelines/shared/_ytt_lib/ci/tasks/update-version-files.sh
+++ pipelines/shared/_ytt_lib/ci/tasks/update-version-files.sh
@@ -11,23 +11,18 @@ update-digest() {
   sed -i -e "s|eirini/${image_name}@sha256:.*$|eirini/${image_name}@${digest}|g" eirini-release/helm/values.yaml
 }
 
-strip-signed-off() {
-  local repo
-  repo="$1"
-
-  grep -v "Signed-off-by" "$repo/.git/commit_message"
-}
-
 generate-commit-message() {
-  local ref commit_msg commit_msg_path
-  commit_msg_path="$1"
+  local ref commit_msg
   ref=$(cat "./eirini/.git/ref")
-  commit_msg=$(strip-signed-off eirini)
+  commit_msg=$(grep -v "Signed-off-by" "eirini/.git/commit_message")
 
-  echo -e "Update image versions\n" >"$commit_msg_path/message"
-  echo "eirini commit SHA: $ref" >>"$commit_msg_path/message"
-  echo "eirini commit message:" >>"$commit_msg_path/message"
-  echo -e "$commit_msg\n" >>"$commit_msg_path/message"
+  {
+    echo -e "Update image versions\n"
+    echo "eirini commit SHA: $ref"
+    echo "eirini commit message:"
+    echo -e "$commit_msg\n"
+  } >"$COMMIT_MSG_PATH/message"
+
 }
 
 main() {
@@ -35,7 +30,7 @@ main() {
     update-digest "${image}-image" "$image"
   done
 
-  generate-commit-message "$COMMIT_MSG_PATH"
+  generate-commit-message
 
   cp -r "$REPO/." "$REPO_MODIFIED"
 }
