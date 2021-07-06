ab951e7ebe1a06aabfa0ea054151ae7a3f8a1f4f
Georgi Sabev <georgethebeatle@gmail.com>
2021-07-06 14:44:27 +0000

    Remove redundant git commit script

--- pipelines/shared/_ytt_lib/ci/tasks/clean-up-cluster-config.sh
+++ pipelines/shared/_ytt_lib/ci/tasks/clean-up-cluster-config.sh
@@ -17,18 +17,6 @@ remove-cluster-values() {
 }
 
 copy-output() {
-  pushd cluster-state || exit
-  if git status --porcelain | grep .; then
-    echo "Repo is dirty"
-    git add "$CLUSTER_DIR"
-    git config --global user.email "eirini@cloudfoundry.org"
-    git config --global user.name "Come-On Eirini"
-    git commit --all --message "Delete values file for cluster: $CLUSTER_NAME"
-  else
-    echo "Repo is clean"
-  fi
-  popd || exit
-
   cp -r "$CLUSTER_STATE/." "$CLUSTER_STATE_MODIFIED/"
 }
 
