5b4540887d3094832eda37fc7c264f98b42ac01e
Georgi Sabev <georgethebeatle@gmail.com>
2021-07-06 14:57:32 +0000

    Use the shared git_commit task in the egg pipeline

--- pipelines/shared/_ytt_lib/ci/tasks/bump-yarn-packages.sh
+++ pipelines/shared/_ytt_lib/ci/tasks/bump-yarn-packages.sh
@@ -2,25 +2,5 @@
 
 set -euxo pipefail
 
-commit() {
-  pushd eirinidotcf
-  if git status --porcelain | grep .; then
-    echo "Repo is dirty"
-    git add web
-    git --no-pager diff --staged
-    git config --global user.email "eirini@cloudfoundry.org"
-    git config --global user.name "Come-On Eirini"
-    git commit --all --message "Bump yarn packages"
-  else
-    echo "Repo is clean"
-  fi
-  popd
-  cp -r eirinidotcf/. eirinidotcf-updated
-}
-
-main() {
-  yarnpkg upgrade --cwd eirinidotcf/web
-  commit
-}
-
-main
+yarnpkg upgrade --cwd "$REPO/web"
+cp -r "$REPO/." "$REPO_MODIFIED"
