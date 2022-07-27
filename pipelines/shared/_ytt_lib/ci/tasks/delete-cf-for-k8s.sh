3662441bdbbb64e6c78be4d197700da50e9d264c
Kieron Browne <kbrowne@vmware.com>
2022-07-27 14:09:55 +0000

    Remove the eirini acceptance env
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- pipelines/shared/_ytt_lib/ci/tasks/delete-cf-for-k8s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
-export KUBECONFIG="$PWD/kube/config"
-
-kapp delete -a cf --yes
--- pipelines/shared/_ytt_lib/ci/tasks/get-postfacto-credentials-cf4k8s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-export KUBECONFIG="$PWD/kube/config"
-export GOOGLE_APPLICATION_CREDENTIALS="$PWD/kube/service-account.json"
-
-redis_password=$(kubectl get secret postfacto-redis -n postfacto-redis -o json | jq -r '.data | .["redis-password"]' | base64 -d)
-echo "$redis_password" >redis-password/password
